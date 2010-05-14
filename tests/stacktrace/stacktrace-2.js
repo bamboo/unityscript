7b23cc7c7f02852fb517a35a6abbbb1ce2a903ed
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-05-14 19:46:45 -0300

    test would pass if inlining was disabled

--- tests/stacktrace/stacktrace-2.js
+++ tests/stacktrace/stacktrace-2.js
@@ -1,14 +1,16 @@
 /*
-stacktrace-2.js:14
-stacktrace-2.js:8
+stacktrace-2.js:16
+stacktrace-2.js:9
 */
 import UnityScript.Tests;
+import System.Runtime.CompilerServices;
 
 // cuts off the root level of the stacktrace
 var p = new Test();
 
 class Test
 {
+	//@MethodImpl(MethodImplOptions.NoInlining) //uncommenting the line makes it work 
 	function Test ()
 	{
 		throw new System.Exception();
