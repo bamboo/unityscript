d7dc3a65a55159431945e5b4eb634dcf7c03cf77
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-05-24 09:59:47 -0300

    test case for array downcasts

---
+++ tests/pragma/downcast-plus-strict-for-arrays.js
@@ -0,0 +1,19 @@
+/*
+Bar
+*/
+#pragma strict
+#pragma downcast
+
+class Foo {
+}
+
+class Bar extends Foo {
+}
+
+function useBars(bars:Bar[]) {
+	print(bars);
+}
+
+var foos:Foo[] = new Bar[0];
+var bars:Bar[] = foos;
+useBars(foos);
