205ea5916dbc87fe9bff9af30d6f893ecf7167c5
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-05-29 07:35:17 -0300

    little script to check virtual function call performance over non virtual function calls

---
+++ resources/function-call-performance.js
@@ -0,0 +1,33 @@
+import System;
+
+class Foo {
+	public var counter = 0;
+	function Bar() { ++counter; }
+}
+
+class VirtualFoo {
+	public var counter = 0;
+	virtual function Bar() { ++counter; }
+}
+
+class VirtualFoo2 extends VirtualFoo {
+	override function Bar() {}
+}
+
+function time(kind, block: function()) {
+	var calls = 100000000;
+	var start = DateTime.Now;
+	for (var i=0; i<calls; ++i)
+		block();
+	var elapsed = DateTime.Now - start;
+	print(calls + " "  + kind + " in " + elapsed.TotalMilliseconds + "ms.");
+}
+
+var foo = new Foo();
+var virtualFoo = new VirtualFoo();
+
+for (var i = 0; i < 3; ++i) {
+	time("non virtual calls", function() { foo.Bar(); });
+	time("virtual calls", function() { virtualFoo.Bar(); });
+}
+
