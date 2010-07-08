89bba3a8e8dcfe0f1cfc7dfaaa8e9d2428ef7100
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2010-07-08 17:05:47 -0300

    error message has changed

--- tests/error-messages/nolinenumber.js
+++ tests/error-messages/nolinenumber.js
@@ -1,5 +1,5 @@
 /*
-nolinenumber.js(14,18): BCE0101: Return type 'void' cannot be used on a generator. Did you mean 'void*' ? Or use a 'System.Collections.IEnumerable' or 'object'.
+nolinenumber.js(14,18): BCE0101: Return type 'void' cannot be used on a generator. Did you mean 'void*'? You can also use 'System.Collections.IEnumerable' or 'object'.
 */
 
 class A
