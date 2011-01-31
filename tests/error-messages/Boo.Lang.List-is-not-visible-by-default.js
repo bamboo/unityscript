fb379c6ab550e2521dd5eb7a4498d574c61f2663
Rodrigo B. de Oliveira <rodrigobamboo@gmail.com>
2011-01-31 12:04:20 -0200

    suggestions will come from system assemblies first

--- tests/error-messages/Boo.Lang.List-is-not-visible-by-default.js
+++ tests/error-messages/Boo.Lang.List-is-not-visible-by-default.js
@@ -1,4 +1,4 @@
 /*
-Boo.Lang.List-is-not-visible-by-default.js(4,8): BCE0018: The name 'List' does not denote a valid type ('not found'). Did you mean 'Boo.Lang.List'?
+Boo.Lang.List-is-not-visible-by-default.js(4,8): BCE0018: The name 'List' does not denote a valid type ('not found'). Did you mean 'System.Collections.Generic.List'?
 */
 var l: List.<String>;
