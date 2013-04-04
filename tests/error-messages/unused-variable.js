cb7e7843dff518415f82249b2a92e63224e9ba07
Rodrigo B. de Oliveira <rbo@acm.org>
2013-04-04 17:49:14 -0300

    improve unused variable test case

--- tests/error-messages/unused-variable.js
+++ tests/error-messages/unused-variable.js
@@ -1,5 +1,20 @@
 /*
+unused-variable.js(11,7): BCW0003: WARNING: Unused local variable 'unused'.
+unused-variable.js(15,7): BCW0003: WARNING: Unused local variable 'unused'.
+unused-variable.js(19,7): BCW0003: WARNING: Unused local variable 'unused'.
 */
 // Unused member variables should not give any warnings.
 // This warning also shows up when the variable is used in a function.
 var target : String;
+
+function f() {
+  var unused;
+}
+
+function g() {
+  var unused: Object;
+}
+
+function h() {
+  var unused = 42;
+}
